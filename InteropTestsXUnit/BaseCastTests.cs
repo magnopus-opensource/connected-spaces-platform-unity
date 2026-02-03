8d3c4688869f57ba73983506c605bf93b023713c
Elliot Morris <Elliot.Morris@magnopus.com>
2026-02-03 15:02:05 +0000

    Add pointer tests and base cast tests
    Two new interop mechanisms, two new test suites

---
+++ InteropTestsXUnit/BaseCastTests.cs
@@ -0,0 +1,46 @@
+namespace InteropTestsXUnit;
+
+using csp.common;
+using csp.multiplayer;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+public class BaseCastTests
+{
+
+    [Fact]
+    public void ComponentCast()
+    {
+        TempMockScriptRunner MockScriptRunner = new TempMockScriptRunner();
+        LogSystem LogSystem = new LogSystem();
+        OfflineRealtimeEngine RealtimeEngine = new OfflineRealtimeEngine(LogSystem, MockScriptRunner);
+
+        SpaceEntity SpaceEntity = new SpaceEntity(RealtimeEngine, MockScriptRunner, LogSystem);
+        ComponentBase Component = SpaceEntity.AddComponent(ComponentType.Text);
+
+        Assert.NotNull(Component);
+
+        // Cast to the correct subtype
+        TextSpaceComponent TextComponent = TextSpaceComponent.FromBaseCast(Component);
+        Assert.NotNull(TextComponent);
+    }
+
+    [Fact]
+    public void InvalidComponentCastThrows()
+    {
+        TempMockScriptRunner MockScriptRunner = new TempMockScriptRunner();
+        LogSystem LogSystem = new LogSystem();
+        OfflineRealtimeEngine RealtimeEngine = new OfflineRealtimeEngine(LogSystem, MockScriptRunner);
+
+        SpaceEntity SpaceEntity = new SpaceEntity(RealtimeEngine, MockScriptRunner, LogSystem);
+        ComponentBase Component = SpaceEntity.AddComponent(ComponentType.Text);
+
+        Assert.NotNull(Component);
+
+        // Attempt to cast to the wrong subtype
+        var exception = Assert.Throws<ArgumentException>(() => CollisionSpaceComponent.FromBaseCast(Component));
+        Assert.Contains("Failed to cast", exception.Message);
+        Assert.Equal("base", exception.ParamName);
+    }
+}
---
+++ InteropTestsXUnit/PointerTests.cs
@@ -0,0 +1,123 @@
+
+namespace InteropTestsXUnit;
+
+using csp.common;
+using csp.multiplayer;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+public class PointerTests
+{
+
+    [Fact]
+    public async Task SpaceEntityPointersPointToSameMemory()
+    {
+        TempMockScriptRunner MockScriptRunner = new TempMockScriptRunner();
+        LogSystem LogSystem = new LogSystem();
+        OfflineRealtimeEngine RealtimeEngine = new OfflineRealtimeEngine(LogSystem, MockScriptRunner);
+
+        SpaceTransform NewEntityTransform = new SpaceTransform(new Vector3(1, 2, 3), new Vector4(0, 0, 0, 1), new Vector3(2, 3, 4));
+
+        SpaceEntity OriginalEntity = await RealtimeEngine.CreateEntityAsync("OriginalEntity", NewEntityTransform, null);
+
+        Assert.Equal("OriginalEntity", OriginalEntity.GetName());
+        Assert.Equal(new Vector3(1, 2, 3), OriginalEntity.GetPosition());
+
+        // Get the same entity, check that they are equal.
+        SpaceEntity SameEntity = RealtimeEngine.GetEntityByIndex(0);
+
+        //The proxy classes are not equal. If we made SpaceEntity equatable (totally could), this could change.
+        Assert.NotEqual(OriginalEntity, SameEntity);
+
+        //However, the underlying swigCPtr should be the same. Do some reflection dark arts to check
+        var SwigCPtrField = typeof(SpaceEntity)
+        .GetField("swigCPtr", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        HandleRef OriginalEntitySwigCPtrHandle = (HandleRef)SwigCPtrField.GetValue(OriginalEntity);
+        HandleRef SameEntitySwigCPtrHandle = (HandleRef)SwigCPtrField.GetValue(SameEntity);
+
+        Assert.Equal(OriginalEntitySwigCPtrHandle.Handle, SameEntitySwigCPtrHandle.Handle);
+    }
+
+    [Fact]
+    public async Task UpdateChangesPropagateToOriginalProxyObject()
+    {
+        //Leveraging the fact that offline engines are single threaded to keep the test simple.
+
+        TempMockScriptRunner MockScriptRunner = new TempMockScriptRunner();
+        LogSystem LogSystem = new LogSystem();
+        OfflineRealtimeEngine RealtimeEngine = new OfflineRealtimeEngine(LogSystem, MockScriptRunner);
+
+        SpaceTransform NewEntityTransform = new SpaceTransform(new Vector3(1, 2, 3), new Vector4(0, 0, 0, 1), new Vector3(2, 3, 4));
+
+        SpaceEntity OriginalEntity = await RealtimeEngine.CreateEntityAsync("OriginalName", NewEntityTransform, null);
+
+        SpaceEntity? CallbackEntity = null;
+        OriginalEntity.SetUpdateCallback(new ConnectedSpacesPlatformDotNet.UpdateCallback((UpdatedSpaceEntity, UpdateFlags, UpdatedComponentInfoArray) => { CallbackEntity = UpdatedSpaceEntity; }));
+        OriginalEntity.SetPosition(new Vector3(2, 3, 4));
+
+        //Now we have two proxy objects, each point to the same memory and have the same data
+
+        //Both proxy objects are different, but should have the same position.
+        Assert.NotNull(CallbackEntity);
+        Assert.Equal(new Vector3(2, 3, 4), OriginalEntity.GetPosition());
+        Assert.Equal(new Vector3(2, 3, 4), CallbackEntity.GetPosition());
+
+        //Perform a similar update on the callback proxy object, check the original proxy object updates
+        CallbackEntity.SetName("NewName");
+
+        Assert.Equal("NewName", CallbackEntity.GetName());
+        Assert.Equal("NewName", OriginalEntity.GetName());
+    }
+
+    [Fact]
+    public async Task DifferentSpaceEntitiesPointToDifferentMemory()
+    {
+        TempMockScriptRunner MockScriptRunner = new TempMockScriptRunner();
+        LogSystem LogSystem = new LogSystem();
+        OfflineRealtimeEngine RealtimeEngine = new OfflineRealtimeEngine(LogSystem, MockScriptRunner);
+
+        SpaceTransform NewEntityTransform = new SpaceTransform(new Vector3(1, 2, 3), new Vector4(0, 0, 0, 1), new Vector3(2, 3, 4));
+
+        SpaceEntity Entity1 = await RealtimeEngine.CreateEntityAsync("SameName", NewEntityTransform, null);
+        SpaceEntity Entity2 = await RealtimeEngine.CreateEntityAsync("SameName", NewEntityTransform, null);
+
+        //Check that the underlying swig pointers are different
+        var SwigCPtrField = typeof(SpaceEntity)
+        .GetField("swigCPtr", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        HandleRef OriginalEntitySwigCPtrHandle = (HandleRef)SwigCPtrField.GetValue(Entity1);
+        HandleRef SameEntitySwigCPtrHandle = (HandleRef)SwigCPtrField.GetValue(Entity2);
+
+        Assert.NotEqual(OriginalEntitySwigCPtrHandle.Handle, SameEntitySwigCPtrHandle.Handle);
+    }
+
+    [Fact]
+    public async Task MemoryOwnershipDependsOnWhereItemInstantiated()
+    {
+        TempMockScriptRunner MockScriptRunner = new TempMockScriptRunner();
+        LogSystem LogSystem = new LogSystem();
+        OfflineRealtimeEngine RealtimeEngine = new OfflineRealtimeEngine(LogSystem, MockScriptRunner);
+
+        SpaceTransform NewEntityTransform = new SpaceTransform(new Vector3(1, 2, 3), new Vector4(0, 0, 0, 1), new Vector3(2, 3, 4));
+
+        SpaceEntity CreatedByCpp = await RealtimeEngine.CreateEntityAsync("SameName", NewEntityTransform, null);
+
+        // The injection of the RealtimeEngine is suspicious here. I'm not convinced CSP is designed with this
+        // sort of instantiation in mind. Not a concern for the CSharp wrapper, but CSP should break this 
+        // dependency as it creates dangerous assumptions around memory ownership.
+        SpaceEntity CreatedByCSharp = new SpaceEntity(RealtimeEngine, MockScriptRunner, LogSystem);
+
+        //Check underlying memory ownership
+        var SwigCMemOwnField = typeof(SpaceEntity)
+        .GetField("swigCMemOwn", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        bool SwigCMemOwnCreatedByCpp = (bool)SwigCMemOwnField.GetValue(CreatedByCpp);
+        bool SwigCMemOwnCreatedByCSharp = (bool)SwigCMemOwnField.GetValue(CreatedByCSharp);
+
+        Assert.False(SwigCMemOwnCreatedByCpp);
+        Assert.True(SwigCMemOwnCreatedByCSharp);
+    }
+
+
+}
