fdbb1d4e9cf01c3c743f29642a40eebb2ffff9b5
Elliot Morris <Elliot.Morris@magnopus.com>
2026-02-03 14:58:56 +0000

    Add interface tests and IJSScriptRunner mock
    Add a temporary mock so we can test realtime engines and space entities, and the tests for the new interface declarations.

---
+++ InteropTestsXUnit/InterfaceTests.cs
@@ -0,0 +1,70 @@
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+
+
+namespace InteropTestsXUnit;
+
+using csp.common;
+using csp.multiplayer;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+public class InterfaceTests
+{
+    /*
+     * Test types declared as %interface are represented correctly in generated output
+     * At time of writing, this is exclusively multiplayer component interfaces.
+     * ... which to be honest, I doubt are a design that's going to stick around very long.
+     */
+
+    [Fact]
+    public void InterfacesAreCSharpInterface()
+    {
+        // Just test a couple
+        Assert.True(typeof(ITransformComponent).IsInterface);
+        Assert.True(typeof(IVisibleComponent).IsInterface);
+    }
+
+    [Fact]
+    public void HasInterface()
+    {
+        // Don't need to actually instantiate to test this.
+        StaticModelSpaceComponent StaticModelSpaceComponent = new StaticModelSpaceComponent(0, false);
+        Assert.True(StaticModelSpaceComponent is ComponentBase);
+
+        // Look at all these interfaces!
+        Assert.True(StaticModelSpaceComponent is IExternalResourceComponent);
+        Assert.True(StaticModelSpaceComponent is IShadowCasterComponent);
+        Assert.True(StaticModelSpaceComponent is IThirdPartyComponentRef);
+        Assert.True(StaticModelSpaceComponent is ITransformComponent);
+        Assert.True(StaticModelSpaceComponent is IPositionComponent);
+        Assert.True(StaticModelSpaceComponent is IRotationComponent);
+        Assert.True(StaticModelSpaceComponent is IScaleComponent);
+        Assert.True(StaticModelSpaceComponent is IVisibleComponent);
+        Assert.True(StaticModelSpaceComponent is IRenderBehaviourComponent);
+    }
+
+    [Fact]
+    public void CanUseAsInterface()
+    {
+        TempMockScriptRunner MockScriptRunner = new TempMockScriptRunner();
+        LogSystem LogSystem = new LogSystem();
+        OfflineRealtimeEngine RealtimeEngine = new OfflineRealtimeEngine(LogSystem, MockScriptRunner);
+
+        SpaceEntity SpaceEntity = new SpaceEntity(RealtimeEngine, MockScriptRunner, LogSystem);
+
+        StaticModelSpaceComponent StaticModelSpaceComponent = new StaticModelSpaceComponent(LogSystem, SpaceEntity);
+
+        Assert.Equal(StaticModelSpaceComponent.GetPosition(), new Vector3(0, 0, 0));
+        StaticModelSpaceComponent.SetPosition(new Vector3(1, 2, 3));
+        Assert.Equal(StaticModelSpaceComponent.GetPosition(), new Vector3(1, 2, 3));
+
+        // Get the component as an interface, try to do the same thing
+        IPositionComponent PositionComponentInterface = StaticModelSpaceComponent;
+        Assert.Equal(PositionComponentInterface.GetPosition(), new Vector3(1, 2, 3));
+        StaticModelSpaceComponent.SetPosition(new Vector3(2, 4, 6));
+        Assert.Equal(StaticModelSpaceComponent.GetPosition(), new Vector3(2, 4, 6));
+    }
+
+
+}
--- interface/CSP/Common/Interfaces/IJSScriptRunner.i
+++ interface/CSP/Common/Interfaces/IJSScriptRunner.i
@@ -4,3 +4,38 @@
 
 %interface_impl(csp::common::IJSScriptRunner);
 %include "CSP/Common/Interfaces/IJSScriptRunner.h"
+
+
+
+// A temporary empty IJSScriptRunner implementation such that we can test things.
+// Normally you just get the script system and use that as you IJSScriptRunner.
+%inline  %{
+namespace csp
+{
+    namespace common{
+        class TempMockScriptRunner : public csp::common::IJSScriptRunner
+        {
+            public:
+            bool RunScript(int64_t ContextId, const csp::common::String& ScriptText) override
+            {
+                return true;
+            }
+            void RegisterScriptBinding (csp::common::IScriptBinding* /*ScriptBinding*/) override {}
+            void UnregisterScriptBinding(csp::common::IScriptBinding* /*ScriptBinding*/) override {}
+
+            bool BindContext(int64_t /*ContextID*/) override {return true;}
+            bool ResetContext(int64_t /*ContextID*/) override {return true;}
+            void* GetContext(int64_t /*ContextID*/) override {return nullptr;}
+
+            void* GetModule(int64_t /*ContextID*/, const csp::common::String& /*ModuleName*/) override {return nullptr;}
+
+            bool CreateContext(int64_t /*ContextID*/) override {return true;}
+            bool DestroyContext(int64_t /*ContextID*/) override {return true;}
+
+            void SetModuleSource(csp::common::String /*ModuleUrl*/, csp::common::String /*Source*/) override {}
+            void ClearModuleSource(csp::common::String /*ModuleUrl*/) override {}
+        };
+
+    }
+}
+%}
