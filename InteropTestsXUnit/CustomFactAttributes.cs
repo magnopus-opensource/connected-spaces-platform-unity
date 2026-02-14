35aaf5d7e07452c461ef2c253253755d6caa3bab
Elliot Morris <Elliot.Morris@magnopus.com>
2026-02-14 22:44:46 +0000

    Add End to End tests
    Wrote a few end to end tests to verify that our patterns work. These arn't enough to really prove anything, but they did help shake out a few bugs.
    
    As they require live services, and thus VPN access, added a custom attribute to skip tests based on the existence of an environment variable. As I was growing frustrated at the time the async tests were taking, I also guarded the longer running ones of those as well.

---
+++ InteropTestsXUnit/CustomFactAttributes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteropTestsXUnit
+{
+    /* Only runs a test if a certain environment variable is set
+     * Added to avoid running tests that require hitting live services by default */
+    public class EnvironmentFactAttribute : FactAttribute
+    {
+        public EnvironmentFactAttribute(string envVar)
+        {
+            if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable(envVar)))
+                Skip = $"Skipped: environment variable '{envVar}' is not set.";
+        }
+    }
+
+}
